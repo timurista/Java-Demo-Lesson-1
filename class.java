563cdcb87613fb1b0776dfe405660c1e103106ae
Tim Urista <timothy.urista@gmail.com>
2016-02-04 11:13:13 -0700

    added main method
    main method added

--- class.java
+++ class.java
@@ -1,3 +1,5 @@
+public static void main( String[] args) {
 // TODO: replace the greeting below with your own greeting
 System.out.println("Hello world!");
 // TODO: print a second greeting to the user
+}
